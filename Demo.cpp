5ca29ab435c8550ee421c332669f19c9bdd670b1
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2022-11-24 19:26:16 +0100

    Generation procédurale implémentée
    lesgoooo

--- Demo.cpp
+++ Demo.cpp
@@ -9,10 +9,13 @@
 
 #define DB_PERLIN_IMPL
 #include "db_perlin.hpp"
+#include "TileMap.h"
+#include <iostream>
 
 int main() {
-    int unsigned width = 800 ;
-    int unsigned height =  600 ;
+    int unsigned width = 1024 ;
+    int unsigned height =  512 ;
+    
 
     sf::RenderWindow window{
         sf::VideoMode { width, height },"Perlin Noise"
@@ -30,6 +33,11 @@ int main() {
     output.setTexture(texture);
 
     sf::Clock clock;
+    int count = 0;
+    float level[32][16] = {};
+    int level2[512] = {};
+        
+
     while (window.isOpen()) {
         sf::Event ev{};
         while (window.pollEvent(ev)) {
@@ -37,24 +45,36 @@ int main() {
                 window.close();
             }
         }
-
         auto const dt = clock.getElapsedTime().asSeconds();
-        for (auto y = 0; y < height; ++y) {
-            for (auto x = 0; x < width; ++x) {
-                auto const noise = (
+
+        for (auto y = 0; y < 16; ++y) {
+            for (auto x = 0; x < 32; ++x) {
+                
+                level[x][y] = (
                     db::perlin(float(x) / 64.0f, float(y) / 64.0f, dt * 0.25f) * 1.0f +
                     db::perlin(float(x) / 32.0f, float(y) / 32.0f, dt * 0.75f) * 0.5f
-                    ) / 1.5f;
-
-                auto const brightness = sf::Uint8((noise) * 255.0f);
-                image.setPixel(x, y, sf::Color(brightness, brightness, brightness));
+                    ) / 0.25f;
+                if (level[x][y] <= 0) {
+                    level[x][y] = -level[x][y];
+                }
+                if (level[x][y] >= 3) level[x][y] = 3;
+                level2[count] = round(level[x][y]);
+                count++;
             }
         }
+        count = 0;
+
+        
+
+        TileMap map;
+        if (!map.load("tileSet.png", sf::Vector2u(32, 32), level2, 32, 16))
+        return -1;
+
 
         texture.update(image);
 
         window.clear();
-        window.draw(output);
+        window.draw(map);
         window.display();
     }
 }
--- main.cpp
+++ main.cpp
@@ -7,16 +7,3 @@
 //
 //    Game::endGame();
 //}
-
-//#define DB_PERLIN_IMPL
-//#include "db_perlin.hpp"
-//
-//int main() {
-//    float x = 1.8;
-//    float y = 3.2;
-//    float z = 4.5;
-//
-//    float noise1D = db::perlin(x);
-//    float noise2D = db::perlin(x, y);
-//    float noise3D = db::perlin(x, y, z);
-//}
